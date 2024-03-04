5994e82148b3cea54e9b548a37f5b985c2c50608
Channy SAO <saochanny9944@gmail.com>
2024-03-04 21:44:29 +0700

    first commit

---
+++ src/test/java/com/chanty/hrsystem/HrSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chanty.hrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
